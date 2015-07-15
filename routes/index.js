b23e8f0aa95c870e9f3163a6340451b1eb8e645a
ajimenez164 <ajimenez@prueba.es>
2015-07-15 23:40:02 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Get datos del autor
 router.get('/author', function(req, res) {
     res.render('author', {autor: 'Alejandro Jiménez', imagen: '/images/profile.jpg'});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//definiciones de rutas de quizes
+router.get('/quizes',                        quizController.index);
+router.get('/quizes/:quizId(\\d+)',          quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',   quizController.answer);
 
 module.exports = router;
